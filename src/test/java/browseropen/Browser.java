5c8f1054fa5a428cc20a29d8b63ea5f02de06abb
Aswathy <aswathyudaynair@gmail.com>
2023-08-17 14:54:04 +0530

    test123

---
+++ src/test/java/browseropen/Browser.java
@@ -0,0 +1,22 @@
+package browseropen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Browser {
+	
+	WebDriver driver;
+	
+	
+	public static void main(String[] args) throws InterruptedException
+	{
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");		
+        driver.quit();
+
+	}
+
+}
---
+++ src/test/java/browseropen1/Googleclass.java
@@ -0,0 +1,25 @@
+package browseropen1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class Googleclass
+{
+
+
+	@Test()
+	public  void Validlogin() throws InterruptedException
+	{
+	
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		
+	driver.quit();
+	}
+
+}
